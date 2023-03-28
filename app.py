dfe51f9bef7a903bf3204ae760a79fff81e41b15
Guardianff <103484627+Guardianff@users.noreply.github.com>
2023-03-28 12:16:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 #Thank you LazyDeveloper for helping me in this journey !
-#Must Subscribe On YouTube @LazyDeveloperr 
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '@LazyDeveloper'
+    return '@GUARDIANff'
 
 
 if __name__ == "__main__":
